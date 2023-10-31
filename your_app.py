7e344b77b7c4854edaddd4e0810338c7af4be873
gabrielepinto <48219954+gabrielepinto@users.noreply.github.com>
2023-10-31 15:15:21 +0100

    Update your_app.py

--- your_app.py
+++ your_app.py
@@ -45,9 +45,8 @@ for u, v in G.edges:
 
 # Create a Streamlit app
 st.title("Trova percorso al palazzo delle finanze")
-st.subheader("<<--- Selezionare il punto di partenza e di arrivo dal menù in alto a sinistra")
-st.text("n.b. i dati sulla posizione dei corridoio e le stanze utilizzati in questa app sono esemplificativi")
-st.text("al momento l'applicazione produce solo un'immagine statica che può essere ingrandita cliccando sul tasto ingrandisci in alto a destra -->")
+st.subheader("<-- Selezionare il punto di partenza e di arrivo dal menù in alto a sinistra")
+st.text("n.b. i dati attualmente utilizzati sono puramente esemplificativi ")
 
 st.text("per maggiori info: gabriele.pinto@mef.gov.it")
 # Sidebar with user input
